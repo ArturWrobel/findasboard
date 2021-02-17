618e83d488757ad4c2aa1a04204a7caa7053e6ff
ArturWrobel <artur.r.wrobel@gmail.com>
2021-02-17 17:07:01 +0100

    excel read

--- client/src/views/Tables/Tables.jsx
+++ client/src/views/Tables/Tables.jsx
@@ -1,66 +1,69 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 import { makeStyles } from "@material-ui/core/styles";
 import { Grid, Typography, Paper } from "@material-ui/core/"
 import { useDropzone } from 'react-dropzone'
 import * as XLSX from "xlsx"
 
+import Data from "./Data"
 import styles from './styles.js'
+
 
 const useStyles = makeStyles(styles)
 
 const Tables = () => {
-
     const classes = useStyles();
-    const { acceptedFiles, getRootProps, getInputProps } = useDropzone()
-    const files = acceptedFiles.map(file => (
-        <li key={file.path}>
-            {file.path} - {file.size} bytes
-        </li>
-    ))
+    const [data, setData] = useState([])
 
-    const [items, setItems] = useState([]);
+    const onDrop = useCallback((acceptedFiles) => {
+        acceptedFiles.forEach((file) => {
+            const reader = new FileReader()
+            reader.readAsArrayBuffer(file)
 
-    const readExcel = (file) => {
-        const promise = new Promise((resolve, reject) => {
-            const fileReader = new FileReader();
-            fileReader.readAsArrayBuffer(file);
-
-            fileReader.onload = (e) => {
+            reader.onload = (e) => {
                 const bufferArray = e.target.result;
                 const wb = XLSX.read(bufferArray, { type: "buffer" });
                 const wsname = wb.SheetNames[0];
                 const ws = wb.Sheets[wsname];
                 const data = XLSX.utils.sheet_to_json(ws);
-                resolve(data);
+                setData(data)
                 console.log(data);
+                console.log(typeof (data))
+                console.log(data[0]['Project'])
             };
+        })
+    }, [])
 
-            fileReader.onerror = (error) => {
-                reject(error);
-            };
-        });
+    const { acceptedFiles, getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop }, {accept: 'image/jpeg, image/png'})
 
-        promise.then((d) => {
-            setItems(d);
-        });
-    };
+    const files = acceptedFiles.map(file => (
+        <li key={file.path}>
+            {file.path} - {file.size} bytes
+        </li>
+    ))
+    console.log(acceptedFiles.length)
+    console.log('accepted')
+    //const xxx = reader.readAsArrayBuffer(acceptedFiles[0])
+    console.log('after')
+    console.log(acceptedFiles[0])
+    console.log(typeof (files[0]))
+    console.log('state')
+    //console.log(data[0]['Project'])
+    console.log('state')
+
+
 
     return (
         <>
             <Paper elevation={3} className={classes.title}>
                 <Typography variant="h4" >
-                    Leasing Calculator
+                    Read and analyze table
                     </Typography>
             </Paper>
             <Paper elevation={3} className={classes.drag}
                 {...getRootProps({ className: ['dropzone', classes.drag] })}
             >
+                {isDragActive ? "Drop it like it's hot!" : 'Click me or drag a file to upload!'}
                 <input {...getInputProps()}
-                    type="file"
-                    onChange={(e) => {
-                        const file = e.target.files[0];
-                        readExcel(file);
-                    }}
                 />
                 <Typography variant="h5" >
                     Drag and drop here
@@ -70,9 +73,11 @@ const Tables = () => {
                     </Typography>
                 {files}
             </Paper>
-            <Typography variant="h6">
-                {items}
-            </Typography>
+
+            <Paper elevation={3}>
+                {!data ? <Data/> : <Typography variant="h4"> Import Data from file </Typography>}
+                
+            </Paper>
         </>
     )
 }
